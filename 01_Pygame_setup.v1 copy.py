845a65b129f491d63b7dc563974d3b56a3d0b27f
rafaelanggawijaya <anggawijay3@middleton.school.nz>
2025-05-13 12:09:10 +1200

    yes

---
+++ 01_Pygame_setup.v1 copy.py
@@ -0,0 +1,34 @@
+"""01_Pygame_setup V2
+Description: Setting up the UI of the game
+Updates:Changing sizes to find the best dimensions for screen
+By Rafael Anggawijaya 
+"""
+import pygame
+import sys
+
+
+# Starting pygame
+pygame.init()
+
+# Setting up screen dimensions
+WIDTH, HEIGHT = 1000, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Car Ricers")
+
+# Clock for controlling frame rate
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+# Colors
+WHITE = (255, 255, 255)
+
+
+# Main game loop
+while True:
+    # sets the colour of the background
+    screen.fill(WHITE)
+    # quit game (helps testing)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
